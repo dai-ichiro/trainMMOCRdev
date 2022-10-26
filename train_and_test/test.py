93438e9758f5fea5267f3ceb0effed493de78b67
dai-ichiro <53703225+dai-ichiro@users.noreply.github.com>
2022-10-27 01:31:07 +0900

    Add files via upload

---
+++ train_and_test/test.py
@@ -0,0 +1,19 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import sys
+from mmocr.ocr import MMOCR
+
+img = sys.argv[1]
+
+cfg = 'satrn_output/satrn_japanese_cfg.py'
+checkpoint = 'satrn_output/epoch_1.pth'
+
+ocr_model = MMOCR(
+    det = None,
+    recog_config = cfg, 
+    recog_ckpt = checkpoint,
+    device = 'cuda'
+    )
+
+results = ocr_model.readtext(img, print_result=True, show=False)
